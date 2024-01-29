c27f6686103969421f182235c2fc172af111b4c3
jgargar307 <jgargar307@iesmartinezm.es>
2024-01-29 14:56:30 +0100

    ugysfiyws

---
+++ modulo/main.js
@@ -0,0 +1,8 @@
+"use strict"
+import {sumar, restar, multiplicar, dividir} from "./operaciones.js";
+{
+    console.log(sumar(2,3))
+    console.log(restar(2,3))
+    console.log(multiplicar(2,3))
+    console.log(dividir(2,3))
+}
---
+++ modulo/operaciones.js
@@ -0,0 +1,18 @@
+"use strict"
+export function sumar(a,b){
+    return a + b
+}
+
+export function restar(a,b){
+    return a - b
+}
+
+export function multiplicar(a,b){
+    return a + b
+}
+
+export function dividir(a,b){
+    return a / b
+}
+
+
